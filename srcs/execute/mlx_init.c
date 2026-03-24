7b96bea01e5b618268f2e14a6b4daf109ac42416
mariannabrnrd <mariannabernardi04@gmail.com>
2026-03-24 16:00:54 +0100

    sistemato nome texture + trovato bug in path

--- srcs/execute/mlx_init.c
+++ srcs/execute/mlx_init.c
@@ -2,9 +2,10 @@
 
 void    load_texture(t_game *game, t_tex *tex)
 {
+    printf("path: |%s|\n", tex->path);
     tex->img = mlx_xpm_file_to_image(game->mlx.process, tex->path, &tex->w, &tex->h);
     if (!tex->img)
-        exit_game(game, "texture load error");
+        exit_game(game, "texture load error\n");
     tex->data = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len, &tex->endian);
 }
 
@@ -20,10 +21,10 @@ void    set_game(t_game *game)
 {
     game->mlx.process = mlx_init();
     if (!game->mlx.process)
-        ft_error("mlx error");
+        ft_error("mlx error\n");
     game->mlx.win = mlx_new_window(game->mlx.process, game->mlx.width, game->mlx.height, "cub3d");
     if (!game->mlx.win)
-        exit_game(game, "win error");
+        exit_game(game, "win error\n");
     set_texture(game);
     //set_img(game);
 }
